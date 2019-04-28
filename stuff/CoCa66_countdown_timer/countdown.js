75a0a32635e9707fb2f20175f9e7e8ec350ecdae
johvia <48033989+johvia@users.noreply.github.com>
2019-04-28 22:49:35 +1200

    timer

--- stuff/CoCa66_countdown_timer/countdown.js
+++ stuff/CoCa66_countdown_timer/countdown.js
@@ -1,7 +1,48 @@
-function setup() {
-	createCanvas(400,400);
+var counter = 0;
+var timeleft = 10;
+
+var ding;
+
+function preload(){
+	ding = loadSound("assets/ding.wav");
 }
 
-function draw() {
-	background(100);
+
+
+function setup() {
+	noCanvas();
+
+	var params = getURLParams();
+
+
+
+	if (params.minute){
+
+
+	var min = params.minute;
+	timeleft = min * 60;
+
+	}
+	var timer = select('#timer');
+	timer.html(convertSeconds(timeleft - counter));
+
+	var interval = setInterval(timeIt, 1000);
+
+	function timeIt() {
+		counter++;
+		timer.html(convertSeconds(timeleft - counter));
+		if (counter === timeleft){
+			ding.play();
+			clearInterval(interval);
+			// counter = 0;
+		}
+	}
+
+
+}
+
+function convertSeconds(s){
+	var min = floor(s / 60);
+	var sec = s % 60;
+	return nf(min,2) + ':' + nf(sec,2);
 }
