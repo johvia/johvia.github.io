f7b1f07afc05cd869e21e89d091728c0bea7d4f9
johvia <48033989+johvia@users.noreply.github.com>
2019-05-07 03:38:31 +1200

    text that reacts to mouse

---
+++ stuff/CoCa59_steering_behaviors/steering_b.js
@@ -0,0 +1,89 @@
+var font;
+
+var vehicles = [];
+
+var points;
+
+var txt;
+
+var oldtxt;
+
+var pt = [];
+
+var vehicle;
+
+function preload() {
+	font = loadFont('assets/hello_day.otf');
+}
+
+function setup() {
+	createCanvas(600,300);
+	txt = document.getElementById('text').value;
+	oldtxt = document.getElementById('text').value;
+	background(51);
+	// textFont(font);
+	// textSize(148);
+	// fill(255);
+	// noStroke();
+	// text('Johvia', 100, 200);
+
+	// points = font.textToPoints(txt, 100, 200, 148);
+	updatePoints();
+	makeTxt();
+
+}
+
+function draw() {
+
+
+	background(51);
+	for (var i = 0; i < vehicles.length; i++){
+		var v = vehicles[i];
+		v.behaviors();
+		v.update();
+		v.show();
+	}
+	// console.log(txt);
+
+	txt = document.getElementById('text').value;
+
+	if (txt === oldtxt){
+		// console.log("no update");
+	}else {
+		clear();
+
+		cleanArray();
+
+		updatePoints();
+
+		makeTxt();
+	}
+
+}
+
+
+
+function makeTxt() {
+
+	for (var i = 0; i < points.length ;i++) {
+		pt = points[i];
+		vehicle = new Vehicle(pt.x,pt.y);
+		vehicles.push(vehicle);
+
+	}
+}
+
+function updatePoints(){
+	txt = document.getElementById('text').value;
+	oldtxt = document.getElementById('text').value;
+	points = font.textToPoints(txt, 100, 200, 148);
+}
+
+function cleanArray(){
+	for (var i = 0; i < points.length ;i++) {
+		pt = points[i];
+		vehicle = new Vehicle(pt.x,pt.y);
+		vehicles.splice(vehicle,1);
+		pt = [];
+	}
+}
