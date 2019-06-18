da3f83809f187fe59349b6831eb118c2bbeb1241
johvia <48033989+johvia@users.noreply.github.com>
2019-06-19 10:00:43 +1200

    Update sketch.js

--- stuff/CoCa77_recursion/sketch.js
+++ stuff/CoCa77_recursion/sketch.js
@@ -1,22 +1,33 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 600);
 }
 
 function draw() {
   background(100);
   stroke(255);
   noFill();
-  drawCircle(300, height/2, 300);
+  drawCircle(300, (height/8)*3, 300);
 	noLoop();
 }
 
 function drawCircle(x, y, d) {
 
   ellipse(x, y, d);
+  //square(x,y,d);
 	if (d > 2) {
-		
+
 		drawCircle(x + d * 0.5, y, d * 0.5);
 		drawCircle(x - d * 0.5, y, d * 0.5);
+
+
+
+    drawCircle(x , y + d * 0.5, d * 0.5);
+    //drawCircle(x , y - d * 0.5, d * 0.25);
+
+
+
+
+
 
 		//drawCircle(x, y + d * 0.5, d * 0.5);
 
