34fd84dd430f52ae0995f6a24b46d372feb14279
johvia <48033989+johvia@users.noreply.github.com>
2019-07-15 02:28:57 +1200

    Database

--- stuff/Data_and_APIs/module_two/one/index.js
+++ stuff/Data_and_APIs/module_two/one/index.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const dataStore = require('nedb');
 
 const app = express();
 
 app.listen(4040, () => console.log('listening at port 4040'));
 app.use(express.static('public'));
+app.use(express.json({
+  limit: '1mb'
+}));
+
+const database = new dataStore('database.db');
+database.loadDatabase();
+
+app.post('/api', function(request, response) {
+  console.log('I got a request!');
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json({
+    status: 'succes',
+    timestamp: data.timestamp,
+    latitude: data.lat,
+    longitude: data.lon
+  });
+});
