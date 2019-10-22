828dbe22869efbbe259e60c1763ec63c9e7326c5
johvia <48033989+johvia@users.noreply.github.com>
2019-10-22 21:36:55 +1300

    Update hello.py

--- snakes/hello/hello.py
+++ snakes/hello/hello.py
@@ -1,1 +1,23 @@
 print("Hello World");
+
+name = "Jovan";
+
+print("Hello %s!" % name);
+
+age = 17;
+
+print("%s, you are %d years old!" % (name, age))
+
+string_one = "Here is a string";
+
+print(len(string_one));
+
+print(string_one.index("o"));
+
+print(string_one.count("e"));
+
+print(string_one[3:7]);
+
+print(string_one[3:7:2]);
+
+print(string_one[::-1]);
